59c890b9163e1d5aef7a78304eece486164942a0
yosuke-maji <ymaji220@gmail.com>
2023-09-06 13:46:00 +0900

    課題再提出1-4
    課題再提出　1-4

--- JavaScript/1-4/main.js
+++ JavaScript/1-4/main.js
@@ -1,15 +1,17 @@
 for(let number = 1; number <= 100; number++){
-    if( number % 3 === 0 ){
-        console.log("Fizz!");
-        continue;
-    }
+    if( number % 3 === 0 && number % 5 ===0 ){
+        console.log("FizzBuzz!!");
+    } 
     else if( number % 5 === 0 ){
         console.log("Buzz!");
-        continue;
+        
 
-    }else if( number % 3 === 0 && number % 5 ===0 ){
-        console.log("FizzBuzz!!");
-        continue;
+    }else if( number % 3 === 0 ){
+        console.log("Fizz!");
+        
+    }else if( number % 3 !== 0 || number % 5 !== 0){
+        console.log(number);
     }
-    console.log(number);
+    
+        
 }
