912d8659ba45e0e67df655ab7321393de15ab590
yosuke-maji <ymaji220@gmail.com>
2023-09-07 19:12:46 +0900

    課題再提出1-5
    課題再提出1-5

--- JavaScript/1-5/main.js
+++ JavaScript/1-5/main.js
@@ -1,11 +1,11 @@
 function createJuice(fruits){
     console.log(`${fruits}を受け取りました。ジュースにして返します。`);
-    createJuice = `${fruits}ジュース`;
     
-    return createJuice;
+    
+    return `${fruits}ジュース`;
 }
 
-createJuice("みかん"); 
+ 
 
-let orangeJuice = createJuice;
+let orangeJuice = createJuice("みかん");
 console.log( `${orangeJuice}が届きました`);
